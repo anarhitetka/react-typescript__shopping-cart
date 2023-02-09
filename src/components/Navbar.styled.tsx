abfa922f43bf71d253ba4ba81fe10ffb81effbb7
anarhitetka <a.urumovska@gmail.com>
2023-02-09 15:12:46 +0100

    navbar sticky position+add num of items indicator

--- src/components/Navbar.styled.tsx
+++ src/components/Navbar.styled.tsx
@@ -7,13 +7,19 @@ export const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  padding: 20px;
+  padding: 0 20px;
+  height: 80px;
   box-shadow: -3px -3px 16px 3px rgba(0, 0, 0, 0.25);
   -webkit-box-shadow: -3px -3px 16px 3px rgba(0, 0, 0, 0.25);
   -moz-box-shadow: -3px -3px 16px 3px rgba(0, 0, 0, 0.25);
-  @media (max-width: 385px) {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  @media (max-width: 420px) {
     justify-content: center;
     align-items: center;
+    height: auto;
+    padding-bottom: 20px;
     div {
       display: flex;
       flex-direction: column;
@@ -42,17 +48,38 @@ export const NavButton = styled.button`
   &:active {
     color: #aeaf4f;
   }
-  @media (max-width: 385px) {
+  @media (max-width: 420px) {
     width: 80vw;
   }
 `;
 
-export const NavButtonSvg = styled.button`
+export const NavButtonShoppingCart = styled.button`
   background-color: #fff;
-  border: none;
+  border: 1px solid grey;
+  margin-right: 20px;
+  padding: 10px 12px;
+  border-radius: 50px;
+  position: relative;
   cursor: pointer;
-  padding-right: 20px;
-  @media (max-width: 385px) {
-    padding-right: 0;
+  @media (max-width: 420px) {
+    margin-right: 0;
   }
+  &:hover {
+    background-color: #ebebeb;
+  }
+`;
+
+export const NumOfItemsInCart = styled.div`
+  background-color: #f72121;
+  color: white;
+  height: 1.5rem;
+  width: 1.5rem;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  transform: translate(25%, 25%);
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
